7780f9b7d15be9f6b03a925b62f2400c32aee150
Seyam <pain63tartar@gmail.com>
2026-09-24 07:08:04 +0600

    i'm out of my limit, some othertime will fix home section

---
+++ main.js
@@ -0,0 +1,18 @@
+// Swipper script
+const swiper = new Swiper('.mySwiper', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+// Swipper script end
